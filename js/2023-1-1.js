7cc31664dc70fb4b53490a4d27a70621d4d3ecdf
li-02 <2272944319@qq.com>
2023-01-03 20:24:19 +0800

    2023-1-3

--- js/2023-1-1.js
+++
@@ -1,3 +0,0 @@
-let a="Hello"
-console.log(a.charCodeAt(0))
-console.log(String.raw`\n`.length)  
---
+++ js/类型-值-变量.js
@@ -0,0 +1,12 @@
+let a = 123.456
+// console.log(a.charCodeAt(0))
+// console.log(String.raw`\n`.length)  
+// console.log(a.toExponential(2))
+
+
+let i = 10;
+i = "ten"
+// console.log(i)
+var foo = { unique_prop: 1 }, bar = { unique_prop: 2 }, object = {};
+object[foo] = 'value';
+console.log(object[bar]);
